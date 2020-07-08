ef7171c7bfcba7f151c8a0e63b0761ad817e55de
GabrielVoidd <souzagabriel0006@gmail.com>
2020-07-08 15:29:18 -0300

    Adicionando telas e metodos de cadastro e listagem

---
+++ GamesApp/GamesApp/Models/Database.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace GamesApp.Models
+{
+    public class Database
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Jogo>().Wait();
+        }
+
+        public Task<List<Jogo>> GetJogoAsync()
+        {
+            return _database.Table<Jogo>().ToListAsync();
+        }
+
+        public Task<int> SaveJogoAsync(Jogo jogo)
+        {
+            return _database.InsertAsync(jogo);
+        }
+    }
+}
